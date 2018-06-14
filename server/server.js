2c09e8a1558b085bfdb3f82719f6f8a72146b05b
Daniel Luke <whatdauno@gmail.com>
2018-06-14 18:28:13 -0400

    created ENV variables to set up test database

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -16,7 +18,7 @@ var {
 } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
